9870bb89d2c0629814c6f3a87a469f080e72ab6b
Jatin21006 <jatinkumar21006@gmail.com>
2026-07-01 01:26:32 +0530

    Time: 40 ms (35.78%), Space: 37.2 MB (36.06%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> dp(3,0);
-        dp[1]=INT_MIN;
-        dp[2]=INT_MIN;
-        for(int i=0;i<n;i++){
-            int rem=nums[i]%3;
-            if(rem==0){
-                for(int j=0;j<3;j++)dp[j]+=nums[i];
-            }
-            else if(rem==1){
-                vector<int> new_dp(3);
-                new_dp[0]=max(dp[0],dp[2]+nums[i]);
-                new_dp[1]=max(dp[1],dp[0]+nums[i]);
-                new_dp[2]=max(dp[2],dp[1]+nums[i]);
-                dp=new_dp;
-            }
-            else{
-                vector<int> new_dp(3);
-                new_dp[0]=max(dp[0],dp[1]+nums[i]);
-                new_dp[1]=max(dp[1],dp[2]+nums[i]);
-                new_dp[2]=max(dp[2],dp[0]+nums[i]);
-                dp=new_dp;
+        vector<int> dp = {0, INT_MIN, INT_MIN};
 
+        for (int x : nums) {
+            vector<int> ndp = dp;
+
+            for (int r = 0; r < 3; r++) {
+                if (dp[r] == INT_MIN) continue;
+                int nr = (r + x) % 3;
+                ndp[nr] = max(ndp[nr], dp[r] + x);
             }
+
+            dp = ndp;
         }
+
         return dp[0];
     }
 };
