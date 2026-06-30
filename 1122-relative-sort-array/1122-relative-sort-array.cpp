e8582f9faa98f68e890871158014972a23ee0922
Jatin21006 <jatinkumar21006@gmail.com>
2026-06-30 16:35:01 +0530

    Time: 3 ms (18.62%), Space: 11.3 MB (21.29%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st(arr2.begin(),arr2.end());
-        unordered_map<int , int> hash;
+        unordered_map<int,int> pos;
+        for (int x : arr2) pos[x] = 0;
+
         vector<int> complement;
-        for(int i=0;i<arr1.size();i++){
-            if(st.find(arr1[i])!=st.end())hash[arr1[i]]++;
-            else complement.push_back(arr1[i]);
+        for (int x : arr1) {
+            if (pos.count(x))
+                pos[x]++;
+            else
+                complement.push_back(x);
         }
+        
         vector<int> res;
-        for(int i=0;i<arr2.size();i++){
-            res.insert(res.end(),hash[arr2[i]],arr2[i]);
+        for(int x:arr2){
+            res.insert(res.end(),pos[x],x);
         }
         sort(complement.begin(),complement.end());
         res.insert(res.end(),complement.begin(),complement.end());
