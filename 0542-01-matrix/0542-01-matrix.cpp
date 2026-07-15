73a89302108aa75cc87ec887905f9c7da8598c30
Jatin21006 <jatinkumar21006@gmail.com>
2026-07-16 00:56:56 +0530

    Time: 23 ms (40.35%), Space: 38.8 MB (38.18%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> ans(n,vector<int>(m));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;//stores {row,col},distance
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                    ans[i][j]=0;
+                }
+            }
+        }
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int d=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0){
+                    ans[nrow][ncol]=d+1;
+                    vis[nrow][ncol]=1;
+                    q.push({{nrow,ncol},d+1});
+                }
+            }
+
+        }
+        return ans;
+        
+    }
+};
