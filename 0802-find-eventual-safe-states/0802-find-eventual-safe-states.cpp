f55daf7df99b656d0c93e83f428c998594c9a38d
Jatin21006 <jatinkumar21006@gmail.com>
2026-07-26 17:48:47 +0530

    Time: 51 ms (40.58%), Space: 66.2 MB (47.63%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -12,18 +12,18 @@ public:
             indegree[i]=graph[i].size();
             if(indegree[i]==0)q.push(i);
         }
-        vector<int> ans;
         while(!q.empty()){
             int node=q.front();
-            ans.push_back(node);
             q.pop();
             for(auto it:adj[node]){
                 indegree[it]--;
                 if(indegree[it]==0)q.push(it);
             }
         }
-        
-        sort(ans.begin(),ans.end());
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)ans.push_back(i);
+        }
         return ans;
     }
 };
