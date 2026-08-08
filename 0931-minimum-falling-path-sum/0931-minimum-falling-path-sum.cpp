32a4f59e8431b6b0ad1bfcbd0e41753589f193c0
Jatin21006 <jatinkumar21006@gmail.com>
2026-08-08 19:12:47 +0530

    Time: 0 ms (100%), Space: 15.1 MB (47.44%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[0][i]=matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int left=(j==0)?INT_MAX:dp[i-1][j-1];
+                int right=(j==n-1)?INT_MAX:dp[i-1][j+1];
+
+                dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(left,right));
+            }
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            ans=min(ans,dp[n-1][i]);
+        }
+        return ans;
+        
+    }
+};
