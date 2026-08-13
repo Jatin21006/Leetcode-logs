5a96012a22be45568095992b9694eccdc92a0495
Jatin21006 <jatinkumar21006@gmail.com>
2026-08-14 00:54:35 +0530

    Time: 1168 ms (5.08%), Space: 451.3 MB (6.03%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        if(s.size()<10)return {};
+        vector<string> ans;
+        unordered_map<string,int> mp;
+        for(int i=0;i<s.size()-9;i++){
+            mp[s.substr(i,10)]++;
+        }
+        for(auto it:mp){
+            if(it.second>1)ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
