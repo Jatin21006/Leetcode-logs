865915c5317a7ba0c920c3c474d906f3871ec2e1
Jatin21006 <jatinkumar21006@gmail.com>
2026-08-16 00:31:07 +0530

    Time: 0 ms (100%), Space: 10.8 MB (93.82%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> freq(26, 0);
-        int L = 0, maxFreq = 0, maxLen = 0;
-
-        for (int R = 0; R < s.size(); R++) {
-            freq[s[R] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[R] - 'A']);
-
-            while ((R - L + 1) - maxFreq > k) {
-                freq[s[L] - 'A']--;
+        vector<int> freq(26,0);
+        int L=0,maxLen=0,maxFreq=0;
+        for(int R=0;R<s.size();R++){
+            freq[s[R]-'A']++;
+            maxFreq=max(maxFreq,freq[s[R]-'A']);
+            while(R-L+1-maxFreq>k){
+                freq[s[L]-'A']--;
                 L++;
             }
-
-            maxLen = max(maxLen, R - L + 1);
+            maxLen=max(maxLen,R-L+1);
         }
         return maxLen;
+
+
+        
     }
 };
