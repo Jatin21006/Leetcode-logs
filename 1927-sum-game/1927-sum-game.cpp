d12983d5b982a3c8746b1045e838bd7f1961cb5e
Jatin21006 <jatinkumar21006@gmail.com>
2026-08-23 13:00:35 +0530

    Time: 6 ms (25.41%), Space: 14 MB (67.83%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -12,6 +12,8 @@ public:
             if(num[i]=='?')cnt2++;
             else sum2+=num[i]-'0';
         }
+        //agar odd no of ? hue toh alice will always since she makes the last move
+        if ((cnt1 + cnt2) % 2)return true;
         //kis side jyada ? hai
         if(cnt1>cnt2 && sum1>sum2)return true;
         if(cnt2>cnt1 && sum2>sum1)return true;
@@ -19,7 +21,6 @@ public:
         int diff=abs(sum1-sum2);
         //max sum jo alice bana sakti hai
         int A1=((changes+1)/2)*9;
-        
         //max sum jo bob bana sakta hai
         int B1=(changes/2)*9;
         if(A1>diff)return true;
