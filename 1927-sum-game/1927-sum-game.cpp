f7e9caf406b62c219861736bc59823db9cdde5ba
Jatin21006 <jatinkumar21006@gmail.com>
2026-08-23 12:57:49 +0530

    Time: 6 ms (25.41%), Space: 13.9 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n=num.size();
+        int sum1=0,sum2=0;
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<n/2;i++){
+            if(num[i]=='?')cnt1++;
+            else sum1+=num[i]-'0';
+        }
+        for(int i=n/2;i<n;i++){
+            if(num[i]=='?')cnt2++;
+            else sum2+=num[i]-'0';
+        }
+        //kis side jyada ? hai
+        if(cnt1>cnt2 && sum1>sum2)return true;
+        if(cnt2>cnt1 && sum2>sum1)return true;
+        int changes=abs(cnt1-cnt2);
+        int diff=abs(sum1-sum2);
+        //max sum jo alice bana sakti hai
+        int A1=((changes+1)/2)*9;
+        
+        //max sum jo bob bana sakta hai
+        int B1=(changes/2)*9;
+        if(A1>diff)return true;
+        if(B1<diff)return true;
+        return false;
+
+    }
+};
