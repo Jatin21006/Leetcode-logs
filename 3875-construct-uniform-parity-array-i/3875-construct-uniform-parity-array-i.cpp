cad1a551de5619f2891910ddc4a0a9d9760cdb75
Jatin21006 <jatinkumar21006@gmail.com>
2026-09-02 09:39:01 +0530

    Time: 1 ms (10.91%), Space: 30.2 MB (94.95%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        //koi ek bhi odd hai toh odd bana sakte hai pura array
+        //so we can make uniform parity in every case
+        return true;
+        
+    }
+};
